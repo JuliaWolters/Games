98a1f2328978fe635dbf897602030de0ab280a29
Julia Wolters <julia.wolters@icloud.com>
2017-08-04 17:07:11 +0200

    Implementierung von FizzBuzz

--- fizzbuzz/src/main/java/fizzbuzz/FizzBuzzMain.java
+++ fizzbuzz/src/main/java/fizzbuzz/FizzBuzzMain.java
@@ -1,5 +1,14 @@
 package fizzbuzz;
 
+/**
+ * Write a program that prints the numbers from 1 to 100.
+ * But for multiples of three print "Fizz" instead of the number 
+ * and for the multiples of five print "Buzz". 
+ * For numbers which are multiples of both three and five print “FizzBuzz”."
+ * 
+ * @author Julia
+ *
+ */
 public class FizzBuzzMain {
 
 	public static void main(String[] args) {
